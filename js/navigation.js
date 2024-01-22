d1f0d808f73f004225eb901d04ec3f29dd82d87f
David Loop <davidloop@gmail.com>
2024-01-22 09:18:55 -0600

    Update content.json and navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,12 @@ let data = json.navigation;
 export function navigation(element) {
     let ul = [];
     data.forEach( obj => {
-        ul.push('<li><a href=' + obj.nav_url + '><span>' + obj.nav_title + '</span></a></li>');
+        if (window.location.pathname == obj.nav_url) {
+            ul.push('<li><a class="active" href=' + obj.nav_url + '><span>' + obj.nav_title + '</span></a></li>');
+        } else {
+            ul.push('<li><a href=' + obj.nav_url + '><span>' + obj.nav_title + '</span></a></li>');
+        }
+        
     });
     $(element).html('<ul>' + ul.join("") + '</ul>');
 }
--- js/projectsNav.js
+++ js/projectsNav.js
@@ -1,17 +1,38 @@
 import json from '/content.json';
-let data = json.projects;
+let projects = json.projects;
+let headings = json.projects_headings;
 
 export function projectsNav(element) {
+    let heading;
     let ul = [];
-    data.forEach( project => {
-        project.present.forEach( name => {
-            $.each(name, function(i, items) {
-                $.each(items, function(j, item) {
-                    let tabTitle = item.project_title.toLowerCase().replace(/ /g, '');
-                    ul.push('<li><a href=' + '#' + tabTitle + '>' + item.project_title + '</a></li>');
+
+    if (window.location.pathname === "/") {
+        heading = headings[0].projects_heading_recent;
+        projects.forEach( project => {
+            project.present.forEach( name => {
+                $.each(name, function(i, items) {
+                    $.each(items, function(j, item) {
+                        let tabTitle = item.project_title.toLowerCase().replace(/ /g, '');
+                        ul.push('<li><a href=' + '#' + tabTitle + '>' + item.project_title + '</a></li>');
+                    });
                 });
             });
         });
-    });
-    $(element).html('<ul>' + ul.join("") + '</ul>');
+    }
+    
+    if (window.location.pathname === "/past/") {
+        heading = headings[0].projects_heading_past;
+        projects.forEach( project => {
+            project.past.forEach( name => {
+                $.each(name, function(i, items) {
+                    $.each(items, function(j, item) {
+                        let tabTitle = item.project_title.toLowerCase().replace(/ /g, '');
+                        ul.push('<li><a href=' + '#' + tabTitle + '>' + item.project_title + '</a></li>');
+                    });
+                });
+            });
+        });
+    }
+
+    $(element).html('<h3>' + heading + '</h3><ul>' + ul.join("") + '</ul>');
 }
